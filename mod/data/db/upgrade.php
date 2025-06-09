b55ce78e803895ed168911e3f9bd4587bcfeaf72
Luca Bösch <luca.boesch@bfh.ch>
2025-06-09 12:08:58 +0200

    MDL-83985 mod_data: change name to 1333 chars

--- mod/data/db/upgrade.php
+++ mod/data/db/upgrade.php
@@ -41,6 +41,7 @@
 
 function xmldb_data_upgrade($oldversion) {
     global $DB;
+    $dbman = $DB->get_manager();
 
     // Automatically generated Moodle v4.1.0 release upgrade line.
     // Put any upgrade step following this.
@@ -72,6 +73,19 @@ function xmldb_data_upgrade($oldversion) {
 
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2024100706) {
+
+        // Changing precision of field name on table data to (1333).
+        $table = new xmldb_table('data');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field name.
+        $dbman->change_field_precision($table, $field);
+
+        // Data savepoint reached.
+        upgrade_mod_savepoint(true, 2024100706, 'data');
+    }
 
     return true;
 }
