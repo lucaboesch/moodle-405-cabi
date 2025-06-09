3d9334da930cb49635f324b4967e847773312736
Luca Bösch <luca.boesch@bfh.ch>
2025-06-09 22:18:08 +0200

    MDL-83985 mod_page: change name to 1333 chars

--- mod/page/db/upgrade.php
+++ mod/page/db/upgrade.php
@@ -43,6 +43,9 @@
  */
 
 function xmldb_page_upgrade($oldversion) {
+    global $DB;
+    $dbman = $DB->get_manager();
+
     // Automatically generated Moodle v4.1.0 release upgrade line.
     // Put any upgrade step following this.
 
@@ -57,6 +60,19 @@ function xmldb_page_upgrade($oldversion) {
 
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2024100706) {
+
+        // Changing precision of field name on table page to (1333).
+        $table = new xmldb_table('page');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field name.
+        $dbman->change_field_precision($table, $field);
+
+        // Page savepoint reached.
+        upgrade_mod_savepoint(true, 2024100706, 'page');
+    }
 
     return true;
 }
