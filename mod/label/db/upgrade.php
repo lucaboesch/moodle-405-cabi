55fc014296e140f5c327ac9b6febf95b354b7f57
Luca Bösch <luca.boesch@bfh.ch>
2025-06-09 21:54:10 +0200

    MDL-83985 mod_label: change name to 1333 chars

--- mod/label/db/upgrade.php
+++ mod/label/db/upgrade.php
@@ -44,6 +44,7 @@
 
 function xmldb_label_upgrade($oldversion) {
     global $CFG, $DB;
+    $dbman = $DB->get_manager();
 
     // Automatically generated Moodle v4.1.0 release upgrade line.
     // Put any upgrade step following this.
@@ -102,6 +103,19 @@ function xmldb_label_upgrade($oldversion) {
 
     // Automatically generated Moodle v4.5.0 release upgrade line.
     // Put any upgrade step following this.
+
+    if ($oldversion < 2024100706) {
+
+        // Changing precision of field name on table label to (1333).
+        $table = new xmldb_table('label');
+        $field = new xmldb_field('name', XMLDB_TYPE_CHAR, '1333', null, XMLDB_NOTNULL, null, null, 'course');
+
+        // Launch change of precision for field name.
+        $dbman->change_field_precision($table, $field);
+
+        // Label savepoint reached.
+        upgrade_mod_savepoint(true, 2024100706, 'label');
+    }
 
     return true;
 }
